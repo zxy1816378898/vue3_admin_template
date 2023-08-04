8da3709160a9f901f2482f3edb92becf80475544
风 <79346792+zxy1816378898@users.noreply.github.com>
2023-08-04 17:21:54 +0800

    初始化

---
+++ .history/src/main_20230804164450.js
@@ -0,0 +1,7 @@
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+app.use(ElementPlus, {
+    locale: zhCn
+})
---
+++ .history/src/main_20230804164545.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+createApp(App).mount('#app')
+
+App.use(ElementPlus, {
+    locale: zhCn
+})
